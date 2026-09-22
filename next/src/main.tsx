600d3fba1422d3edbe7db74eb2bbbd368cef6d72
Carlos Velasco <xtreemze@gmail.com>
2026-09-22 14:54:58 +0800

    lint: use explicit TSX import extension

--- next/src/main.tsx
+++ next/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import { App } from "./app";
+import { App } from "./app.tsx";
 import "./styles.css";
 
 const rootElement = document.querySelector<HTMLElement>("#root");
