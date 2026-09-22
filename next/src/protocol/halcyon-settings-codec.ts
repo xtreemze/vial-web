aa3d140128be87ec0563528d23b2864f8bdfedab
Carlos Velasco <xtreemze@gmail.com>
2026-09-22 16:16:48 +0800

    feat: validate settings protocol version

--- next/src/protocol/halcyon-settings-codec.ts
+++ next/src/protocol/halcyon-settings-codec.ts
@@ -1,6 +1,7 @@
 import {
   assertAcknowledged,
   assertByte,
+  assertProtocolVersion,
   decodeUint16,
   encodeUint16,
   frame,
@@ -81,8 +82,10 @@ function getCapabilitiesRequest(): Uint8Array {
 
 function decodeCapabilities(response: Uint8Array): HalcyonSettingsCapabilities {
   assertAcknowledged(response, COMMAND, OP.getCapabilities, 12);
+  const protocolVersion = readByte(response, 2, "protocol version");
+  assertProtocolVersion("Halcyon settings", protocolVersion, VERSION);
   return {
-    protocolVersion: readByte(response, 2, "protocol version"),
+    protocolVersion,
     flags: readByte(response, 3, "capability flags"),
     layerCount: readByte(response, 4, "layer count"),
     modifierCount: readByte(response, 5, "modifier count"),
